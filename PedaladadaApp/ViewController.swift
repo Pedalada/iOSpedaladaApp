7a578c34889c99ac83a19f3362c77c6d2d45ee2b
BrunoEduardoCSantos <bruno.c.santos@ist.utl.pt>
2016-07-25 22:10:08 +0100

    Pin point location in a map

--- PedaladadaApp/ViewController.swift
+++ PedaladadaApp/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import ArcGIS
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,AGSMapViewLayerDelegate  {
+    let kDefaultMap = "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer"
+  
+    @IBOutlet weak var mapView: AGSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let mapUrl = NSURL(string: kDefaultMap)
+        let tiledLyr = AGSTiledMapServiceLayer(URL: mapUrl)
+        self.mapView.addMapLayer(tiledLyr, withName:"Tiled Layer")
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        //Zooming to an initial envelope with the specified spatial reference of the map.
+        //2. Set the map view's layer delegate
+        self.mapView.layerDelegate = self
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func mapViewDidLoad(mapView: AGSMapView!) {
+        //do something now that the map is loaded
+        //for example, show the current location on the map
+        mapView.locationDisplay.startDataSource()
+    }
+    
 
 }
 
